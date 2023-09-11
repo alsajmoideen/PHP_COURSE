9acb5f50c23608608306bdc52c9549ef8613700e
Alsaj Moideen <alsajmoideen@gmail.com>
2023-09-11 19:55:08 +0530

    06 Topic Added

--- 06_DisplayData.php
+++ 06_DisplayData.php
@@ -13,7 +13,7 @@ if($conn->connect_error){
 echo "Connection Success<br>";
 
 // SQL Query
-$sql = 'SELECT EMP_ID,EMP_NAME,EMP_SALARY FROM EMPLOYEE';
+$sql = 'SELECT * FROM EMPLOYEE';
 
 // Table Display Here
 if(mysqli_query($conn,$sql)){
@@ -24,8 +24,16 @@ if(mysqli_query($conn,$sql)){
 
 $result = mysqli_query($conn,$sql);
 
+// Displaying the Data
 if(mysqli_num_rows($result)>0){
     // Output Data of each row
-    
+    while($row = mysqli_fetch_assoc($result)){
+        echo "EMP ID: " . $row["EMP_ID"]."<br>"."EMP_NAME: " . $row["EMP_NAME"]."<br>"."EMP_ADDRESS: " . $row["EMP_ADDRESS"]."<br>". 
+        "EMP_SALARY: " . $row["EMP_SALARY"]."<br>"."JOIN_DATE: " . $row["JOIN_DATE"]."<br>";
+    }
+}else{
+    echo "0 Results";
 }
+
+mysqli_close($conn);
 ?>
